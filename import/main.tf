96814069c678025e923fbbb0f4009d5fe5bf9ec0
srilakshmicloud <prasannakukunuri53@gmail.com>
2025-11-19 15:54:18 +0530

    terraform

--- import/main.tf
+++ import/main.tf
@@ -1,4 +1,11 @@
 #skeleton block
 resource "aws_instance" "demo" {
-  
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [
+    "sg-08da864ed7845ff7e"
+  ]
+  tags = {
+    Name = "import-demo-change"
+  }
 }
